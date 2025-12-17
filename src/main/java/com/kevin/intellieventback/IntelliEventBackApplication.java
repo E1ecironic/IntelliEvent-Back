3b94c4f616f8a80ae65ce5042ddc0892afdcd78a
zhangkaiwen <2118038404@qq.com>
2025-12-17 10:15:03 +0800

    Initial commit

---
+++ src/main/java/com/kevin/intellieventback/IntelliEventBackApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.intellieventback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliEventBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliEventBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevin/intellieventback/IntelliEventBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.intellieventback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliEventBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
